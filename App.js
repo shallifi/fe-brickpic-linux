3e24af4af02763866c95ec617800f48614c7391c
Benjamin <shallifi@hotmail.com>
2024-06-11 16:34:58 -0400

    camera is working on laptop but not on andriod

---
+++ App.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import HomePage from './src/screens/HomePage';
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator initialRouteName="Home">
+//                 <Stack.Screen name="Home" component={HomePage} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//     },
+//     title: {
+//         fontSize: 24,
+//         fontWeight: 'bold',
+//         marginBottom: 16,
+//     },
+// });
+
+// export default App;
+
+
